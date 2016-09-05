a84912d4b6b45471b15137f8ae5a5bc29f2b3068
badar <badarshahzad54@gmail.com>
2016-09-05 23:59:19 +0500

    In main java Getting Screen size added

--- Franchise Software/src/application/Main.java
+++ Franchise Software/src/application/Main.java
@@ -3,6 +3,8 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -23,6 +25,13 @@ public class Main extends Application {
 
 			Parent root = FXMLLoader.load(getClass().getResource("/application/AddEntryLayout.fxml"));
 			Scene scene = new Scene(root,1280,677);
+			
+			//Getting the screen boundries to visible  bounds of the mains sceen
+			Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+			primaryStage.setX(primaryScreenBounds.getMinX());
+			primaryStage.setY(primaryScreenBounds.getMinY());
+			primaryStage.setWidth(primaryScreenBounds.getWidth());
+			primaryStage.setHeight(primaryScreenBounds.getHeight());
 			
 			scene.getStylesheets().add(getClass().getResource("Main.css").toExternalForm());
 			primaryStage.setScene(scene);
